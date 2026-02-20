7cd23bd5911805ca69dc2a46923d65f3c4dba47e
Anubhav Gautam <anubhavgautam9720169985@gmail.com>
2026-02-20 09:23:51 +0530

    INTIALIZING THE VALUE OF TOUCH PIN IN INTEGER

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,14 @@
 /**
  * @file main.ino
  * @brief Embedded Touch Detection System using TTP223
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author ANUBHAV GAUTAM
+ * @date 2026-01-25
  *
  * @details
  * Reads digital touch input from TTP223 sensor
  * and displays structured output via Serial Monitor.
  */
-
- // TODO 1:
- // Define touch sensor digital pin (Use pin 2)
+int touchPin = 3;
 
  // TODO 2:
  // Create variable to store touch state
